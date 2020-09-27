a9069becb51b8aaa8820ff4855b3be2317e517b1
jsoehnli <jsoehnli@silver.cpi.cs.odu.edu>
2020-09-27 21:30:23 +0000

    Got it all working on the silver server. next step, test back on a windows computer

--- app/config/settings.py
+++ app/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '207p722dreag@to!rrisniyhstyrx3jgi9+5b9tf-#k22vmv82'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['411silverf20.cs.odu.edu', 'localhost', '127.0.0.1', '.localhost', '[::1]']
 
 
 # Application definition
