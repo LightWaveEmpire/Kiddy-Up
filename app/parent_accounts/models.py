dd3802530f4838428f5829ddff36be2c48348f75
Jeremy Soehnlin <jsoeh001@odu.edu>
2020-09-29 20:55:41 -0400

    Added some starting models (DB schema) in the parent_accounts/models.py

--- app/parent_accounts/models.py
+++ app/parent_accounts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Child(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthday = models.DateField()
+    grade = models.IntegerField()
+# ...
+
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=30)
+    location = models.CharField(max_length=50)
+    date = models.DateField()
+# ...
+
+
+class Reward(models.Model):
+    name = models.CharField(max_length=30)
+    point_value = models.IntegerField()
+# ...
