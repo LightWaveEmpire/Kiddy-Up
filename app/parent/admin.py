0ffbe91f8c16bf3dd4fcaf6e24bbcce41233ea63
Desmond Chamber <jam2bklyn@gmail.com>
2020-10-04 12:28:57 -0400

    made updates

--- app/parent/admin.py
+++ app/parent/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+#from .models import *
+
 # Register your models here.
+
+#admin.site.register(Child)
+#admin.site.register(Task)
+#admin.site.register(Event)
+#admin.site.register(Reward)
--- app/parent/models.py
+++ app/parent/models.py
@@ -10,9 +10,21 @@ class Child(models.Model):
     grade = models.IntegerField()
 # ...
 
-
 class Task(models.Model):
+    STATUS =(
+            ('Pending', 'Pending'),
+            ('Inprogress', 'Inprogress'),
+            ('Completed', 'Completed'),
+            ('Missed', 'Missed'),
+    )
     task_name = models.CharField(max_length=30)
+    location = models.CharField(max_length=50)
+    date = models.DateField()
+    status = models.CharField(max_length=50, choices=STATUS)
+# ...
+
+class Event (models.Model):
+    event_name = models.CharField(max_length=30)
     location = models.CharField(max_length=50)
     date = models.DateField()
 # ...
