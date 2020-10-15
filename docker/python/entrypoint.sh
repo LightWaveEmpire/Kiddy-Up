5df53b3d614d0e44bbc41deb37a6650c9eb27329
jsoehnli <jsoehnli@cs.odu.edu>
2020-10-14 21:53:30 -0400

    Update entrypoint.sh to make parent migrations before project migrate

--- docker/python/entrypoint.sh
+++ docker/python/entrypoint.sh
@@ -12,8 +12,7 @@ then
 fi
 
 python manage.py flush --no-input
-#python manage.py migrate auth
-#python manage.py migrate parent
+python manage.py makemigrations parent
 python manage.py migrate
 
 
