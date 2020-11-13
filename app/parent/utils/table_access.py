5a6e5ff703ee49f69503e1fa471e9d378f171ece
rsayoc <rsayoc@cs.odu.edu>
2020-11-12 19:28:27 -0500

    added one function and documentation

--- app/parent/utils/table_access.py
+++ app/parent/utils/table_access.py
@@ -1,8 +1,12 @@
 from django.core.exceptions import ObjectDoesNotExist
 from parent.models import Child, Parent
 
+'''
+Gets a child object that matches the parent and string name.
 
-#get child function by searching with string
+:param pobject: parent object of the child
+:param string_name: name of the child that is being searched
+'''
 def get_child(pobject, string_name)
     try:
         c = Child.objects.get(parent=pobject, name=string_name)
--- app/parent/utils/task_factory.py
+++ app/parent/utils/task_factory.py
@@ -1,6 +1,20 @@
 from parent.models import Child, Task, Original_Task
 
-#create child tasks from original_tasks
+'''
+Creates Original_Task entries from a list if they do not already exist
+
+:param p_object: parent that owns the tasks
+:param listOfEvents: list of strings that will be turned into otasks
+'''
+def create_otasks_from_list(p_object, listOfEvents)
+    for item in listOfEvents
+        if not Original_Task.objects.filter(otask=item).exists()
+            t = Original_Task(parent=p_object, otask=item)
+            t.save()
+
+'''
+Create child tasks from Original_Task table if they do not already exist
+'''
 def create_child_tasks_from_otask()
     for ot in Original_Task.objects.all()
         if not ot.has_created_task()
