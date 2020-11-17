be56938cce55783ac1cb395e121ae3d0a0d6ce75
rsayoc <rsayoc@cs.odu.edu>
2020-11-16 19:47:48 -0500

    completed reward_access.complete_task()

--- app/parent/utils/reward_system.py
+++ app/parent/utils/reward_system.py
@@ -1,11 +1,22 @@
-
+from parent.models import Child, Parent, Task
+from parent.utils import table_access
 
 '''
 Call this to mark a task as completed for a child.
 
-:param p_object: the parent of the child
 :param c_object: the child that owns the task
 :param t_object: the task object to be completed
 '''
-def complete_task():
-    pass
+def complete_task(c_object, t_object):
+    #give points to the child
+    c = c_object
+    t = t_object
+    old_points = c.current_points
+    new_points = t.point_value
+    
+    c.current_points = old_points + new_points
+    c.save()
+ 
+    #mark the task as completed
+    t.status = "COMP"
+    t.save()
