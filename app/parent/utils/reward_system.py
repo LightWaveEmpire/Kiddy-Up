4686c4856a779c66ba45a137d32675fbbc4401da
rsayoc <rsayoc@cs.odu.edu>
2020-11-16 19:28:31 -0500

    added a placeholder for reward_system.complete_task()

---
+++ app/parent/utils/reward_system.py
@@ -0,0 +1,11 @@
+
+
+'''
+Call this to mark a task as completed for a child.
+
+:param p_object: the parent of the child
+:param c_object: the child that owns the task
+:param t_object: the task object to be completed
+'''
+def complete_task():
+    pass
