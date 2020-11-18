884b7707d0f9e3e481a8e74fe4b3e5704b2d33e8
rsayoc <rsayoc@cs.odu.edu>
2020-11-17 21:08:14 -0500

    missing colon added.

--- app/parent/utils/table_access.py
+++ app/parent/utils/table_access.py
@@ -7,7 +7,7 @@ Gets a child object that matches the parent and string name.
 :param pobject: parent object of the child
 :param string_name: name of the child that is being searched
 '''
-def get_child(pobject, string_name)
+def get_child(pobject, string_name):
     try:
         c = Child.objects.get(parent=pobject, name=string_name)
     except ObjectDoesNotExist:
