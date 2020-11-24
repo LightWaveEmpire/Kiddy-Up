237052e5eda58197c072161d1676bdc37b9c8c78
rsayoc <rsayoc@cs.odu.edu>
2020-11-24 16:03:53 -0500

    Fixed Attribute error related to custom user.

--- app/parent/models.py
+++ app/parent/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from parent.utils import entity_extraction
 
+
+User = get_user_model()
 
 class User(AbstractUser):
     is_parent = models.BooleanField(default=False)
